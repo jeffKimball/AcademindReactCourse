222c64e42f7e66d9cdb2a48604a49e7ca3c7436c
J Kimball <jeffkimballis@gmail.com>
2023-10-26 16:48:11 -0500

    Output Dynamic values

--- starting-project/src/App.jsx
+++ starting-project/src/App.jsx
@@ -1,4 +1,7 @@
+import reactImg from './assets/react-core-concepts.png'
+
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+
 
 function genRandomInt(max) {
   return Math.floor(Math.random() * (max + 1));
@@ -9,7 +12,7 @@ function Header() {
 
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={reactImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
         {descriptiveTerm} React concepts you will need for almost any app you are going to build!
