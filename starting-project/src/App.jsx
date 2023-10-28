23e8d6dee7049eba277eb911ea78a76bc4185488
jkimball <jeffkimballis@gmail.com>
2023-10-27 20:57:32 -0500

    refactor Header and CoreConcepts componenets

--- starting-project/src/App.jsx
+++ starting-project/src/App.jsx
@@ -1,34 +1,12 @@
-import reactImg from './assets/react-core-concepts.png'
+
 import { CORE_CONCEPTS } from "./data";
 
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+import Header from './components/Header';
+import CoreConcept from "./components/CoreConcept";
 
 
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
 
-function Header() {
-  const descriptiveTerm = reactDescriptions[genRandomInt(2)]
 
-  return (
-    <header>
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {descriptiveTerm} React concepts you will need for almost any app you are going to build!
-      </p>
-    </header>
-  );
-}
-
-function CoreConcept({image, title, description}) {
-  return <li>
-    <img src={image} alt={title} />
-    <h3>{title}</h3>
-    <p>{description}</p>
-  </li>
-}
 
 function App() {
   return (
