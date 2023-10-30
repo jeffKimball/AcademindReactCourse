7d7f2ce5f605c857c596896141ef6a57ddd07de9
J Kimball <jeffkimballis@gmail.com>
2023-10-30 16:26:32 -0500

    set up conditional content for selected tab in lower menu section

--- starting-project/src/App.jsx
+++ starting-project/src/App.jsx
@@ -1,16 +1,31 @@
-import { useState } from "react";
-import { CORE_CONCEPTS } from "./data";
-import { EXAMPLES } from "./data";
-import Header from './components/Header/Header';
-import CoreConcept from "./components/CoreConcept";
-import TabButton from "./components/TabButton";
+import { useState } from "react"
+import { CORE_CONCEPTS,EXAMPLES  } from "./data"
+import Header from './components/Header/Header'
+import CoreConcept from "./components/CoreConcept"
+import TabButton from "./components/TabButton"
 
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState('components')
+  const [selectedTopic, setSelectedTopic] = useState('')
 
   const handleSelect = (selectedButton) => {
     setSelectedTopic(selectedButton)
+  }
+
+  let tabContent = <p>Please select a topic</p>
+
+  if(selectedTopic){
+    tabContent = (
+      <div id="tab-content">            
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>
+            {EXAMPLES[selectedTopic].code}
+          </code>
+        </pre>
+      </div>
+    )
   }
 
   return (
@@ -34,15 +49,7 @@ function App() {
             <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
             <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
-          <div id="tab-content">
-            <h3>{EXAMPLES[selectedTopic].title}</h3>
-            <p>{EXAMPLES[selectedTopic].description}</p>
-            <pre>
-              <code>
-                {EXAMPLES[selectedTopic].code}
-              </code>
-            </pre>
-          </div>
+          {tabContent}
         </section>      
       </main>
     </div>
