9025712719aa97b64c48679683b5abcce0a350fb
jkimball <jeffkimballis@gmail.com>
2023-11-04 21:22:35 -0500

    initialize tic-tac-toe project

--- starting-project/src/components/Tabs.jsx
+++ starting-project/src/components/Tabs.jsx
@@ -1,6 +1,6 @@
-// If the interface requires a wrapper other than menu, set the ButtonsContainer
-// to the appropriate element: 'div','aside', or a custome component.
-// ex: ButtonsContainer =  Section
+// If the interface requires a wrapper other than menu, set the ButtonsContainer to 
+// the appropriate element: 'div','aside', or a custom component from Example.jsx
+// custom component example: ButtonsContainer =  Section 
 // Of course the Section component would have to be imported as well
 
 const Tabs = ({children, buttons, ButtonsContainer = 'menu'}) => {    
