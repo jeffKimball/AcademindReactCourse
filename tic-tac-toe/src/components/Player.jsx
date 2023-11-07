323b685d48fd6ed36fcd5bc24f0c5bdc29471690
jkimball <jeffkimballis@gmail.com>
2023-11-06 19:55:39 -0600

    set up rows and columns for game board

--- tic-tac-toe/src/components/Player.jsx
+++ tic-tac-toe/src/components/Player.jsx
@@ -1,24 +1,29 @@
 import { useState } from 'react'
 
-const Player = ({name, symbol}) => {
+const Player = ({initialName, symbol}) => {
+    const [playerName, setPlayerName] = useState(initialName)
     const [isEditing, setIsEditing] = useState(false)
 
     const handleEditClick = () => {
         setIsEditing((prevState) => !prevState)
     }
 
-    let playerName = <span className="player-name">{name}</span>    
+    const handleNameChange = (event) => {
+        setPlayerName(event.target.value)
+    }
+
+    let editablePlayerName = <span className="player-name">{playerName}</span>    
     let buttonText = "Edit"
 
     if(isEditing) {
-        playerName = <input type="text" value={name} required/>
+        editablePlayerName = <input type="text" value={playerName} onChange={handleNameChange} required/>
         // buttonText='Save'
     }
 
     return (
         <li>
             <span className="player">
-                {playerName}
+                {editablePlayerName}
                 <span className="player-symbol">{symbol}</span>
             </span>
             <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
