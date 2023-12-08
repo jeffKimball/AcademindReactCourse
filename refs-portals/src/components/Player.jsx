aa3ea1136e5b7f75cb033a3d4aaaf5a3bc77111a
J Kimball <jeffkimballis@gmail.com>
2023-12-08 16:42:42 -0600

    set up event listeners on form for entereing a name

--- refs-portals/src/components/Player.jsx
+++ refs-portals/src/components/Player.jsx
@@ -1,10 +1,35 @@
+import { useState } from 'react'
+
 export default function Player() {
+  const [enteredPlayerName, setEnteredPlayerName] = useState('');
+  const [displayName, setDisplayName] = useState('unknown entity');
+  const [submitted, setSubmitted] = useState(false);
+
+  const handleChange = (e) => {
+    setEnteredPlayerName(e.target.value);
+  };
+
+  const handleSubmit = () => {
+    setDisplayName(enteredPlayerName);
+    setSubmitted(true);
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      handleSubmit();
+    }
+  };
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {submitted ? displayName : 'unknown entity'}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input 
+          value={enteredPlayerName} 
+          onChange={handleChange} 
+          onKeyDown={handleKeyDown}
+        />
+        <button onClick={handleSubmit}>Set Name</button>
       </p>
     </section>
   );
